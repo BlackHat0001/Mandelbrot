6e13c0d6c474863a756c9cfc64d9bdc1cba6acdd
BlackHat <danielantonio2003@gmail.com>
2021-02-10 09:14:53 -0800

    Add files via upload

--- Mandelbrot/src/mandelbrot/Mandelbrot.java
+++ Mandelbrot/src/mandelbrot/Mandelbrot.java
@@ -31,50 +31,94 @@ import java.awt.Canvas;
 *
 * @author danma
 */
-public class Mandelbrot extends Canvas
-{
-    static int xValue;
-    static int yValue;
-    static double zoomLevel;
+public class Mandelbrot extends Canvas {
+    static int xValue = 10;
+    static int yValue = 10;
+    static double zoomLevel = 1.0;
     static int Xconvert;
     static int Iconvert;
     static String[][] pos;
     static int[][] rateToInfinity;
     static int reso;
-    static double step;
-    static double Xstart;
-    static double Xstop;
-    static double Istart;
-    static double Istop;
-    static double coordXAxis;
-    static double coordIAxis;
+    static double step = 0.0025;
+    static double Xstart = -2.0;
+    static double Xstop = 1.0;
+    static double Istart = -1.0;
+    static double Istop = 1.0;
+    static double coordXAxis = Math.abs(0.0 - Xstart);
+    static double coordIAxis = Math.abs(0.0 - Istart);
     
-    static Color c;
-    static JFrame jfx;
-    static JTextField label1;
+    static Color c = Color.black;
+    static JFrame jfx = new JFrame();
+    static JTextField label1 = new JTextField();
     
-    static JPanel jf;
+    static JPanel jf = jf = new JPanel() {
+             RenderingHints QUALITY_RENDER = new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+             RenderingHints ANTIALIASING = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            
+            @Override
+            protected void paintComponent( Graphics g) {
+                 Graphics2D gx = (Graphics2D)g;
+                gx.addRenderingHints(this.ANTIALIASING);
+                gx.addRenderingHints(this.QUALITY_RENDER);
+                yValue = 0;
+                while (yValue < Iconvert) {
+                    xValue = 0;
+                    while (xValue < Xconvert) {
+                         double rti = rateToInfinity[yValue][xValue];
+                         double rtl = rti / reso;
+                         double C = rtl * 255.0;
+                        int B = (int)(C % 256.0 * 255.0);
+                        int G = (int)(C / 256.0 % 256.0 * 10000.0);
+                        int R = (int)(C / 258.0 * 10000.0);
+                        if (R > 255) {
+                            R = 255;
+                        }
+                        if (G > 255) {
+                            G = 255;
+                        }
+                        if (B > 255) {
+                            B = 255;
+                        }
+                        c = new Color(R, G, B);
+                        if (rti >= reso) {
+                            c = new Color(0, 0, 0);
+                        }
+                        if (pos[yValue][xValue] == "XX") {
+                            gx.setColor(c);
+                            gx.fillRect(xValue, yValue, 1, 1);
+                        }
+                        else {
+                            gx.setColor(c);
+                            gx.fillRect(xValue, yValue, 1, 1);
+                        }
+                        ++xValue;
+                    }
+                    ++yValue;
+                }
+            }
+        };;
     
-    public static void main(final String[] args) throws IOException {
-        final JPanel mousepanel = generateSet(false, 0.0, 0.0, 0.0);
+    public static void main( String[] args) throws IOException {
+         JPanel mousepanel = generateSet(false, 0.0, 0.0, 0.0);
         jfx.setDefaultCloseOperation(3);
         jf.updateUI();
         mousepanel.addMouseListener(new MouseAdapter() {
             @Override
-            public void mousePressed(final MouseEvent e) {
-                final Point xy = mousepanel.getMousePosition();
+            public void mousePressed( MouseEvent e) {
+                 Point xy = mousepanel.getMousePosition();
                 zoomLevel = zoomLevel * 1.0 / 2.0;
                 System.out.println(xy.x + " " + xy.y);
             }
             
             @Override
-            public void mouseReleased(final MouseEvent e) {
+            public void mouseReleased( MouseEvent e) {
             }
         });
     }
     
-    public static JPanel generateSet(final boolean target, double xToTarget, double yToTarget, final double zoomLevel) throws IOException {
-        final long startTime = System.currentTimeMillis();
+    public static JPanel generateSet( boolean target, double xToTarget, double yToTarget,  double zoomLevel) throws IOException {
+         long startTime = System.currentTimeMillis();
         System.out.println(" x: " + coordXAxis + " i: " + coordIAxis);
         reso = 2000;
         Xconvert = (int)((Xstop - Xstart) / step);
@@ -105,7 +149,7 @@ public class Mandelbrot extends Canvas
                 
                 
                 n = new Complex(0.0, 0.0);
-                final Complex c = new Complex(x, i);
+                 Complex c = new Complex(x, i);
                 int passes = 0;
                 boolean breakLoop = false;
                 
@@ -137,7 +181,7 @@ public class Mandelbrot extends Canvas
             }
             ++xCount;
         }
-        final long endTime = System.currentTimeMillis();
+         long endTime = System.currentTimeMillis();
         System.out.println("Size: " + Xconvert * Iconvert);
         System.out.println("Iterations: " + reso);
         System.out.println("Number times looped: " + totalLoop);
@@ -150,68 +194,8 @@ public class Mandelbrot extends Canvas
         return jf;
     }
     
-    public void updateGraphics(final int i, final int j) {
-    }
     
-    static {
-        xValue = 10;
-        yValue = 10;
-        zoomLevel = 1.0;
-        step = 0.0025;
-        Xstart = -2.0;
-        Xstop = 1.0;
-        Istart = -1.0;
-        Istop = 1.0;
-        coordXAxis = Math.abs(0.0 - Xstart);
-        coordIAxis = Math.abs(0.0 - Istart);
-        c = Color.black;
-        jfx = new JFrame();
-        label1 = new JTextField();
-        jf = new JPanel() {
-            final RenderingHints QUALITY_RENDER = new RenderingHints(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-            final RenderingHints ANTIALIASING = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            
-            @Override
-            protected void paintComponent(final Graphics g) {
-                final Graphics2D gx = (Graphics2D)g;
-                gx.addRenderingHints(this.ANTIALIASING);
-                gx.addRenderingHints(this.QUALITY_RENDER);
-                yValue = 0;
-                while (yValue < Iconvert) {
-                    xValue = 0;
-                    while (xValue < Xconvert) {
-                        final double rti = rateToInfinity[yValue][xValue];
-                        final double rtl = rti / reso;
-                        final double C = rtl * 255.0;
-                        int B = (int)(C % 256.0 * 255.0);
-                        int G = (int)(C / 256.0 % 256.0 * 10000.0);
-                        int R = (int)(C / 258.0 * 10000.0);
-                        if (R > 255) {
-                            R = 255;
-                        }
-                        if (G > 255) {
-                            G = 255;
-                        }
-                        if (B > 255) {
-                            B = 255;
-                        }
-                        c = new Color(R, G, B);
-                        if (rti >= reso) {
-                            c = new Color(0, 0, 0);
-                        }
-                        if (pos[yValue][xValue] == "XX") {
-                            gx.setColor(c);
-                            gx.fillRect(xValue, yValue, 1, 1);
-                        }
-                        else {
-                            gx.setColor(c);
-                            gx.fillRect(xValue, yValue, 1, 1);
-                        }
-                        ++xValue;
-                    }
-                    ++yValue;
-                }
-            }
-        };
-    }
+    
+    
+    
 }
